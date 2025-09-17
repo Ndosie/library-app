1a879b19e5de3bd1599cbb69c9dc85350620c363
Esther Ndosi <estherndosi24@gmail.com>
2025-09-17 14:59:21 +0300

    Adding page styles with dummy data

---
+++ scripts/script.js
@@ -0,0 +1,83 @@
+const myLibrary = [
+    {
+        uuid: 19101919197,
+        name: 'Mabala the farmer',
+        author: 'Fathom Igwe',
+        pages: 356,
+        read: false
+    },
+    {
+        uuid: 19101919196,
+        name: 'Mabala the farmer',
+        author: 'Fathom Igwe',
+        pages: 356,
+        read: true
+    },
+    {
+        uuid: 19101919195,
+        name: 'Mabala the farmer',
+        author: 'Fathom Igwe',
+        pages: 356,
+        read: false
+    },
+    {
+        uuid: 19101919193,
+        name: 'Mabala the farmer',
+        author: 'Fathom Igwe',
+        pages: 356,
+        read: false
+    },
+    {
+        uuid: 19101919194,
+        name: 'Mabala the farmer',
+        author: 'Fathom Igwe',
+        pages: 356,
+        read: false
+    }
+]
+const contentDiv = document.querySelector('.content')
+const addBtn = document.querySelector('#add_button')
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector("dialog button");
+
+function Book(name, author, pages, read=false) {
+    this.uid = crypto.randomUUID()
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.toggleReadStatus = function () {
+    this.read = this.read ? false : true
+}
+
+function addBookToLibrary(name, author, pages, read) {
+    const book = new Book(name, author, pages, read)
+    myLibrary.push(book)
+}
+
+function displayBooks() {
+    let cards = ``
+    for (let i = 0; i < myLibrary.length; i++) {
+        let toCheck = myLibrary[i].read ? "checked" : ""
+        cards += `<div id=${myLibrary[i].uuid} class="card">
+            <p>Book: ${myLibrary[i].name}</p>
+            <p>Author: ${myLibrary[i].author}</p>
+            <p>Pages: ${myLibrary[i].pages}</p>
+            <label>Read?</label>
+            <input type="checkbox" ${toCheck}/>
+        </div>`
+    }
+    contentDiv.innerHTML = cards
+}
+
+addBtn.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+displayBooks()
