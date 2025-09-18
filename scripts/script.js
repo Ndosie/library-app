03bf04d097b9b3eed54e99657fbb18dcdb0df23a
Esther Ndosi <estherndosi24@gmail.com>
2025-09-18 13:38:38 +0300

    Adding the logic to the application Implemented adding book function Implemented reseting delete buttons and check boxes after adding or deleting a book functions Implemented changing of read status function Added delete button and styled it

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,9 @@
-const myLibrary = [
-    {
-        uuid: 19101919197,
-        name: 'Mabala the farmer',
-        author: 'Fathom Igwe',
-        pages: 356,
-        read: false
-    },
-    {
-        uuid: 19101919196,
-        name: 'Mabala the farmer',
-        author: 'Fathom Igwe',
-        pages: 356,
-        read: true
-    },
-    {
-        uuid: 19101919195,
-        name: 'Mabala the farmer',
-        author: 'Fathom Igwe',
-        pages: 356,
-        read: false
-    },
-    {
-        uuid: 19101919193,
-        name: 'Mabala the farmer',
-        author: 'Fathom Igwe',
-        pages: 356,
-        read: false
-    },
-    {
-        uuid: 19101919194,
-        name: 'Mabala the farmer',
-        author: 'Fathom Igwe',
-        pages: 356,
-        read: false
-    }
-]
+let myLibrary = []
 const contentDiv = document.querySelector('.content')
-const addBtn = document.querySelector('#add_button')
+const addBtn = document.querySelector('#add-button')
 const dialog = document.querySelector("dialog");
-const cancelBtn = document.querySelector("#cancel-button");
+const addForm = document.querySelector('#add-form')
+const cancelBtn = document.querySelector('#cancel-button')
 
 function Book(name, author, pages, read=false) {
     this.uid = crypto.randomUUID()
@@ -58,26 +23,74 @@ function addBookToLibrary(name, author, pages, read) {
 }
 
 function displayBooks() {
+    if (myLibrary.length === 0) {
+        contentDiv.textContent = "No books to display. Kindly add"
+        return
+    }
     let cards = ``
     for (let i = 0; i < myLibrary.length; i++) {
         let toCheck = myLibrary[i].read ? "checked" : ""
-        cards += `<div id=${myLibrary[i].uuid} class="card">
+        cards += `<div id=${myLibrary[i].uid} class="card">
             <p>Book: ${myLibrary[i].name}</p>
             <p>Author: ${myLibrary[i].author}</p>
             <p>Pages: ${myLibrary[i].pages}</p>
-            <label>Read?</label>
-            <input type="checkbox" ${toCheck}/>
+            <label for="name">Read?</label>
+            <input type="checkbox" name="read" class="read" ${toCheck}/>
+            <span class="material-icons delete-button">delete</span>
         </div>`
     }
     contentDiv.innerHTML = cards
+    resetCheckboxes()
+    resetDeleteButtons()
+}
+
+function resetCheckboxes() {
+    const readChkboxes = document.querySelectorAll('.read')
+    readChkboxes.forEach((chkBox) => {
+        chkBox.addEventListener('change', function(e) {
+            const bookId = e.target.parentElement.id
+            const bookIndex = myLibrary.findIndex(obj => obj.uid === bookId)
+            myLibrary[bookIndex].toggleReadStatus()
+        })
+    })
+}
+
+function resetDeleteButtons() {
+    const deleteBtns = document.querySelectorAll('.delete-button')
+    deleteBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            const bookId = e.target.parentElement.id
+            const remained = myLibrary.filter(obj => obj.uid !== bookId)
+            myLibrary = remained
+            displayBooks()
+        })
+    })
 }
 
 addBtn.addEventListener('click', () => {
     dialog.showModal();
 })
 
-cancelBtn.addEventListener("click", () => {
-    dialog.close();
-});
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    dialog.close()
+})
+
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(addForm)
+    const name = formData.get("name")
+    const author = formData.get("author")
+    const pages = formData.get("pages")
+    const read = formData.get("read") === "on" ? true : false
+    if (name === "" || author === "" || pages === "") {
+        alert("Please enter all the fields.")
+    } else {
+        addBookToLibrary(name, author, pages, read)
+        addForm.reset()
+        dialog.close()
+    }
+    displayBooks()
+})
 
 displayBooks()
